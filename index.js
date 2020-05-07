9ef22d8a4860f45ba2745a960622602b054011e1
Pownthep Laokhunthot <pownthepofficial@gmail.com>
2020-05-07 20:32:53 +0700

    added basic socket.io app

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
         console.log('user disconnected');
+    });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
     });
 });
 

7b420590debdb17750ea02f08d0a75c74e4d659a
Pownthep Laokhunthot <pownthepofficial@gmail.com>
2020-05-07 20:23:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
